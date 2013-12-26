b08aacc38b11dba287789ea1d366b3bebe7382fd
zeronickname <maemo@gman.me.uk>
2013-12-26 15:22:07 +0000

    updates to support forecast.io

--- server/weather-script.py
+++ server/weather-script.py
@@ -1,66 +1,44 @@
-#!/usr/bin/python2
-
-# Kindle Weather Display
-# Matthew Petroff (http://www.mpetroff.net/)
-# September 2012
-
-import urllib2
-from xml.dom import minidom
-import datetime
+from time import gmtime, strftime
+import forecastio
+import textwrap
 import codecs
 
+def main():
 
 
-#
-# Download and parse weather data
-#
+    api_key = "YOUR_FORECAST.IO_API_KEY"
+    lat = 51.477
+    lng = 0.0000
 
-# Fetch data (change lat and lon to desired location)
-weather_xml = urllib2.urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=39.3286&lon=-76.6169&format=24+hourly&numDays=4&Unit=e').read()
-dom = minidom.parseString(weather_xml)
+    forecast = forecastio.load_forecast(api_key, lat, lng)
+    by_day = forecast.daily()
 
-# Parse temperatures
-xml_temperatures = dom.getElementsByTagName('temperature')
-highs = [None]*4
-lows = [None]*4
-for item in xml_temperatures:
-    if item.getAttribute('type') == 'maximum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            highs[i] = int(values[i].firstChild.nodeValue)
-    if item.getAttribute('type') == 'minimum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            lows[i] = int(values[i].firstChild.nodeValue)
+    # Open SVG to process
+    output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
 
-# Parse icons
-xml_icons = dom.getElementsByTagName('icon-link')
-icons = [None]*4
-for i in range(len(xml_icons)):
-    icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
+    # Insert icons and temperatures
+    output = output.replace('ICON_ONE',by_day.data[0].icon).replace('ICON_TWO',by_day.data[1].icon).replace('ICON_THREE',by_day.data[2].icon).replace('ICON_FOUR',by_day.data[3].icon)
+    output = output.replace('HIGH_ONE',str(int(round(by_day.data[0].temperatureMax)))).replace('HIGH_TWO',str(int(round(by_day.data[1].temperatureMax)))).replace('HIGH_THREE',str(int(round(by_day.data[2].temperatureMax)))).replace('HIGH_FOUR',str(int(round(by_day.data[3].temperatureMax))))
+    output = output.replace('LOW_ONE',str(int(round(by_day.data[0].temperatureMin)))).replace('LOW_TWO',str(int(round(by_day.data[1].temperatureMin)))).replace('LOW_THREE',str(int(round(by_day.data[2].temperatureMin)))).replace('LOW_FOUR',str(int(round(by_day.data[3].temperatureMin))))
 
-# Parse dates
-xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+    #insert summary of day text
+    summary=textwrap.wrap(forecast.hourly().summary,55)
+    if len(summary) > 1  :
+        wrappedsummary = summary[0] + '<tspan x="25" dy="25">' + summary[1] + '</tspan>'
+    else:
+        wrappedsummary = summary[0]
+
+    output = output.replace('TODAY_SUMMARY',wrappedsummary)
+
+    # Insert days of week
+    output = output.replace('DAY_THREE',by_day.data[2].time.strftime('%A')).replace('DAY_FOUR',by_day.data[3].time.strftime('%A'))
+
+    #Insert timestamp
+    output = output.replace('DATE_TIME', strftime("%H:%M %d/%m", gmtime()))
+
+    # Write output
+    codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
 
 
-
-#
-# Preprocess SVG
-#
-
-# Open SVG to process
-output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
-
-# Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
-output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
-output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
-
-# Insert days of week
-one_day = datetime.timedelta(days=1)
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
-
-# Write output
-codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+if __name__ == "__main__":
+    main()
