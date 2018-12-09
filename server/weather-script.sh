d35feaba7f7c3658d5170e99a209b0db29993ce1
Txomin <txomin.jimenez@gmail.com>
2018-12-09 17:35:45 +0000

    complete server bash script for cron

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$(dirname "$0")"
 
 python weather-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output.png weather.png
-# cp -f weather.png /media/usbhdd1tb/others/weather_display/weather.png
+pngcrush -c 0 -ow weather-script-output.png
+cp -f weather-script-output.png /var/www/html/weather/weather.png
