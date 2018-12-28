9a85bf35ef9d1d85f288ef63cb6930a6e21352c6
Txomin Jiménez Díaz <txomin@nextail.co>
2018-12-28 18:16:54 +0100

    add main endpoint for display service

--- server/eink-display-server.py
+++ server/eink-display-server.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect
 
 import photo_frame
 import weather
 
 app = Flask(__name__)
 
+# init app modules
 photo_frame.init_app(app)
 weather.init_app(app)
+
+# main
+@app.route('/e-ink-display')
+def display_image():
+  return redirect(get_display_module())
+
+def get_display_module():
+  now = datetime.now()
+  if now.minute >= 30:
+    return "/photo_frame"
+  else:
+    return "/weather"
