fe6f5d08146e7dab3a2f52487c0d9b9e62d592df
Txomin Jiménez Díaz <txomin@nextail.co>
2020-10-03 08:36:38 +0200

    add error 404 and 500 handlers in web server

--- server/eink-display-server.py
+++ server/eink-display-server.py
@@ -1,5 +1,7 @@
+import codecs
 from datetime import datetime
 from flask import Flask, redirect
+import subprocess
 
 import photo_frame
 import weather
@@ -29,3 +31,39 @@ def write_heartbeat():
     with open(HEARTBEAT_FILE, 'w') as file:
         heartbeat_data = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
         file.write(heartbeat_data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  print(e)
+  output = build_error_response('ERROR', 'Resource not found')
+  return output, 404, {'Content-Type': 'image/png'}
+
+@app.errorhandler(500)
+def internal_error(e):
+  print(e)
+  output = build_error_response('ERROR', 'Internal Server Error')
+  return output, 500, {'Content-Type': 'image/png'}
+
+def build_error_response(error_title, error_message_1, error_message_2 = ''):
+  output = codecs.open("warning.svg", 'r', encoding='utf-8').read()
+  output = output.replace('#LINE_1', error_title)
+  output = output.replace('#LINE_2', error_message_1)
+  output = output.replace('#LINE_3', error_message_2)
+  codecs.open('/tmp/warning.svg', 'w', encoding='utf-8').write(output)
+  shell_run(f"rsvg-convert --background-color=white -o /tmp/warning.png /tmp/warning.svg")
+  shell_run(f"pngcrush -c 0 -ow /tmp/warning.png")
+
+  return open("/tmp/warning.png", 'rb').read()
+
+def shell_run(command):
+    output = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True
+    )
+    stdout, stderr = output.communicate()
+    if stderr is not None:
+        raise RuntimeError(f"Shell command failed: {stderr}")
+
+    return stdout.decode("utf-8")
