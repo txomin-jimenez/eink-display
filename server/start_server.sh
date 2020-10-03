1c6a195392624bd49fc038cb06cd6debca90cdcc
Txomin Jiménez Díaz <txomin@nextail.co>
2020-10-03 11:13:30 +0200

    launch nook watcher service with server

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,3 +1,16 @@
 # /bin/bash
+
+# turn on bash's job control
+set -m
+
+# Start the web server process and put it in the background
 echo Starting Eink-Display server...
-gunicorn -w 2 -b 0.0.0.0:8000 eink-display-server:app
+gunicorn -w 2 -b 0.0.0.0:8000 eink-display-server:app &
+
+# Start the nook watcher process
+echo Starting Nook Watcher service...
+python nook-watcher.py
+
+# now we bring the server process back into the foreground
+# and leave it there
+fg %1
